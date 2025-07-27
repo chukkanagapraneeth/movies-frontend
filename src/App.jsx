5cbd84216fbf15cfcbcc2e117428798d8164dfee
Shin <cnagapraneeth@gmail.com>
2025-07-28 00:39:50 +0530

    Imported mantine css. Displays movies in card format and buttons open details of movies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import {
-  Container,
   Title,
   Image,
   Card,
   Text,
-  SimpleGrid,
   Group,
   Badge,
   Button,
+  Grid,
 } from "@mantine/core";
 
 export default function App() {
@@ -22,45 +21,52 @@ export default function App() {
   }, []);
 
   return (
-    <Container>
+    <div className="App">
       <Title order={1} my="md">
         🎬 Movies
       </Title>
-      <br />
-      <SimpleGrid cols={3} spacing="lg">
-        {movies.slice(0, 3).map((movie) => (
-          <Card shadow="sm" padding="lg" radius="md" withBorder key={movie.id}>
-            <Card.Section>
-              <Image
-                src="https://picsum.photos/200"
-                height={160}
-                alt="Norway"
-              />
-            </Card.Section>
-
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>{movie.title}</Text>
-              <Badge color="pink">{movie.yearOfRelease}</Badge>
-            </Group>
-
-            <Text c="dimmed" size="sm">
-              Genres: {movie.genres.join(", ")}
-            </Text>
-
-            <Button
-              color="blue"
-              fullWidth
-              mt="md"
+      <Grid justify="space-around">
+        {movies.map((movie) => (
+          <Grid.Col style={{ maxWidth: 350 }} sm={4} xs={4}>
+            <Card
+              shadow="sm"
+              padding="lg"
               radius="md"
-              component="a"
-              href={`https://localhost:7200/api/movies/${movie.id}`}
-              target="_blank"
+              withBorder
+              key={movie.id}
             >
-              Click here for more details
-            </Button>
-          </Card>
+              <Card.Section>
+                <Image
+                  src="https://picsum.photos/200"
+                  height={160}
+                  alt="Norway"
+                />
+              </Card.Section>
+
+              <Group justify="space-between" mt="md" mb="xs">
+                <Text fw={500}>{movie.title}</Text>
+                <Badge color="pink">{movie.yearOfRelease}</Badge>
+              </Group>
+
+              <Text c="dimmed" size="sm">
+                Genres: {movie.genres.join(", ")}
+              </Text>
+
+              <Button
+                color="blue"
+                fullWidth
+                mt="md"
+                radius="md"
+                component="a"
+                href={`https://localhost:7200/api/movies/${movie.id}`}
+                target="_blank"
+              >
+                Click here for more details
+              </Button>
+            </Card>
+          </Grid.Col>
         ))}
-      </SimpleGrid>
-    </Container>
+      </Grid>
+    </div>
   );
 }
